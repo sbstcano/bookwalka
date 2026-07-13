ce9d55b233228300eef0a5c19735182bbbda7435
Sicktrigger <sbstcano@gmail.com>
2026-07-13 11:32:10 +0200

    improve styles

--- desktop-app/main.js
+++ desktop-app/main.js
@@ -134,6 +134,7 @@ function createWindow() {
     minWidth: 800,
     minHeight: 600,
     title: "Bookwalka Desktop",
+    icon: path.join(__dirname, 'bell.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
